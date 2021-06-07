2894d1e88fe676607c4102f579a78cdea7631c02
Sakhineti Praveena <84781653+SakhinetiPraveena@users.noreply.github.com>
2021-06-07 11:37:21 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+
+  return (
+    <div className="note">
+      <h1>Javascript and React.js </h1>
+      <p> This is an amazing bootcamp taken by Shaurya Sinha.
+        We cover everything from scratch
+      </p>
+    </div>
+  );
+} 
+
+export default Note;
